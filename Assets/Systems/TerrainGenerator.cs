319d0018fdde3dfd3178c60a1a5fe418f69b266b
Sir Dudard <sir.dudard@theknightsofunity.com>
2021-08-28 17:58:46 +0200

    Code updated after DudeiNoise update

--- Assets/Systems/TerrainGenerator.cs
+++ Assets/Systems/TerrainGenerator.cs
@@ -103,7 +103,7 @@ namespace DudeiTerrain
             
             currentSettings.positionOffset = noiseSpaceOffset;
             
-            noiseTexture.GenerateNoiseForChanelAsync(currentSettings, NoiseTextureChannel.RED, this, onCompleted);
+            noiseTexture.GenerateNoiseForChanel(currentSettings, NoiseTextureChannel.RED, this, onCompleted);
         }
 
         private int GetSimplifiedTerrainResolution(int fullMeshResolution, int lod)
